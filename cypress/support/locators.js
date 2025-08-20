ee6da9395559b30321f7b5fe0ce37f90c554adb5
Lucas Santana <lucassantan017@gmail.com>
2025-08-19 23:46:06 -0300

    checkout info, checkout overview and checkout complete page locators

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -17,6 +17,25 @@ const locators = {
     CART: {
         ITEM_NAME: '[data-test="inventory-item-name"]',
         PRICE: '[data-test="inventory-item-price"]',
+    },
+    CHECKOUT_INFO: {
+        CHECKOUT_PAGE: '[data-test="checkout"]',
+        FIRST_NAME: '[data-test="firstName"]',
+        LAST_NAME: '[data-test="lastName"]',
+        POSTAL_CODE: '[data-test="postalCode"]',
+        BTN_CONTINUE: '[data-test="continue"]'
+    },
+    CHECKOUT_OVERVIEW: {
+        ITEM_NAME: '[data-test="inventory-item-name"]',
+        ITEM_PRICE: '[data-test="inventory-item-price"]',
+        SUB_TOTAL: '[data-test="subtotal-label"]',
+        PRICE_TOTAL: '[data-test="total-label"]',
+        BTN_FINISH: '[data-test="finish"]'
+    },
+    CHECKOUT_COMPLETE: {
+        DONE_ICON: '[data-test="pony-express"]',
+        THNKS_TXT: '[data-test="complete-header"]',
+        COMPLETE_TXT: '[data-test="complete-text"]'
     }
 
 }
